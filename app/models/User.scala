2a714139d4cd20a9026798239ba7438861f05245
veechauhan <vineet.chauhan@knoldus.com>
2022-05-18 16:22:11 +0530

    only 1

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,10 +1,13 @@
 package models
 
-import play.api.libs.json.{ Json, OFormat }
+import play.api.libs.json.{Json, OFormat}
+
 case class User(
-               email:Option[String],
-               name:String
+                 email: Option[String],
+                 name: String
                )
+
+case  class Person(name:String)
 
 object  User {
   implicit val userFormat: OFormat[User] = Json.format[User]
