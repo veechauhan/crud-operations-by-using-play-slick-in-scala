87fb91680baf9dd4c88ebe4a81565145f914318d
Vineet Chauhan <86958280+veechauhan@users.noreply.github.com>
2022-05-18 14:47:11 +0530

    Update User.scala

--- app/models/User.scala
+++ app/models/User.scala
@@ -5,7 +5,7 @@ case class User(
                email:Option[String],
                name:String
                )
-case  class Person(name:String)
+case  class Persons(name:String)
 object  User {
   implicit val userFormat: OFormat[User] = Json.format[User]
 }
